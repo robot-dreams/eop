64e320793cfc2eaadade715c5fd69ad00a268065
Elliott Jin <elliott.jin@gmail.com>
2015-12-08 21:06:03 -0800

    Order selection using natural ordering (+ tests)

--- my_selection.h
+++ my_selection.h
@@ -1,6 +1,7 @@
 #ifndef MY_SELECTION
 #define MY_SELECTION
 
+#include <cassert>
 #include "my_intrinsics.h"
 #include "my_type_functions.h"
 
@@ -265,7 +266,7 @@ const Domain(R)& select_1_4(const Domain(R)& a,
                             const Domain(R)& d,
                             R r)
 {
-    if (r(b, a)) return select_1_4_ab(b, a, c, d ,r);
+    if (r(b, a)) return select_1_4_ab(b, a, c, d, r);
     return              select_1_4_ab(a, b, c, d, r);
 }
 
@@ -277,7 +278,7 @@ Domain(R)& select_1_4(Domain(R)& a,
                       Domain(R)& d,
                       R r)
 {
-    if (r(b, a)) return select_1_4_ab(b, a, c, d ,r);
+    if (r(b, a)) return select_1_4_ab(b, a, c, d, r);
     return              select_1_4_ab(a, b, c, d, r);
 }
 
@@ -743,6 +744,658 @@ inline Domain(R)& my_median_5(Domain(R)& a,
                               R r)
 {
     return my_select_2_5(a, b, c, d, e, r);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_0_2(const T& a,
+                    const T& b)
+{
+    // Precondition:
+    //     weak_ordering(r)
+    if (b < a) return b;
+    return a;
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_0_2(T& a,
+              T& b)
+{
+    // Precondition:
+    //     weak_ordering(r)
+    if (b < a) return b;
+    return a;
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_1_2(const T& a,
+                    const T& b)
+{
+    // Precondition:
+    //     weak_ordering(r)
+    if (b < a) return a;
+    return b;
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_1_2(T& a,
+              T& b)
+{
+    // Precondition:
+    //     weak_ordering(r)
+    if (b < a) return a;
+    return b;
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_0_3(const T& a,
+                    const T& b,
+                    const T& c)
+{
+    return select_0_2(select_0_2(a, b), c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_0_3(T& a,
+              T& b,
+              T& c)
+{
+    return select_0_2(select_0_2(a, b), c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_2_3(const T& a,
+                    const T& b,
+                    const T& c)
+{
+    return select_1_2(select_1_2(a, b), c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_2_3(T& a,
+              T& b,
+              T& c)
+{
+    return select_1_2(select_1_2(a, b), c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_1_3_ab(const T& a,
+                       const T& b,
+                       const T& c)
+{
+    if (!(c < b)) return b;
+    return select_1_2(a, c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_1_3_ab(T& a,
+                 T& b,
+                 T& c)
+{
+    if (!(c < b)) return b;
+    return select_1_2(a, c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_1_3(const T& a,
+                    const T& b,
+                    const T& c)
+{
+    if (b < a) return select_1_3_ab(b, a, c);
+    return            select_1_3_ab(a, b, c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_1_3(T& a,
+              T& b,
+              T& c)
+{
+    if (b < a) return select_1_3_ab(b, a, c);
+    return            select_1_3_ab(a, b, c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_0_4(const T& a,
+                    const T& b,
+                    const T& c,
+                    const T& d)
+{
+    return select_0_2(select_0_3(a, b, c), d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_0_4(T& a,
+              T& b,
+              T& c,
+              T& d)
+{
+    return select_0_2(select_0_3(a, b, c), d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_3_4(const T& a,
+                    const T& b,
+                    const T& c,
+                    const T& d)
+{
+    return select_1_2(select_2_3(a, b, c), d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_3_4(T& a,
+              T& b,
+              T& c,
+              T& d)
+{
+    return select_1_2(select_2_3(a, b, c), d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_1_4_ab_cd(const T& a,
+                          const T& b,
+                          const T& c,
+                          const T& d)
+{
+    if (c < a) return select_0_2(a, d);
+    return            select_0_2(b, c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_1_4_ab_cd(T& a,
+                    T& b,
+                    T& c,
+                    T& d)
+{
+    if (c < a) return select_0_2(a, d);
+    return            select_0_2(b, c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_1_4_ab(const T& a,
+                       const T& b,
+                       const T& c,
+                       const T& d)
+{
+    if (d < c) return select_1_4_ab_cd(a, b, d, c);
+    return            select_1_4_ab_cd(a, b, c, d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_1_4_ab(T& a,
+                 T& b,
+                 T& c,
+                 T& d)
+{
+    if (d < c) return select_1_4_ab_cd(a, b, d, c);
+    return            select_1_4_ab_cd(a, b, c, d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_1_4(const T& a,
+                    const T& b,
+                    const T& c,
+                    const T& d)
+{
+    if (b < a) return select_1_4_ab(b, a, c, d);
+    return            select_1_4_ab(a, b, c, d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_1_4(T& a,
+              T& b,
+              T& c,
+              T& d)
+{
+    if (b < a) return select_1_4_ab(b, a, c, d);
+    return            select_1_4_ab(a, b, c, d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_2_4_ab_cd(const T& a,
+                          const T& b,
+                          const T& c,
+                          const T& d)
+{
+    if (d < b) return select_1_2(a, d);
+    return            select_1_2(b, c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_2_4_ab_cd(T& a,
+                    T& b,
+                    T& c,
+                    T& d)
+{
+    if (d < b) return select_1_2(a, d);
+    return            select_1_2(b, c);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_2_4_ab(const T& a,
+                       const T& b,
+                       const T& c,
+                       const T& d)
+{
+    if (d < c) return select_2_4_ab_cd(a, b, d, c);
+    return            select_2_4_ab_cd(a, b, c, d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_2_4_ab(T& a,
+                 T& b,
+                 T& c,
+                 T& d)
+{
+    if (d < c) return select_2_4_ab_cd(a, b, d, c);
+    return            select_2_4_ab_cd(a, b, c, d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& select_2_4(const T& a,
+                    const T& b,
+                    const T& c,
+                    const T& d)
+{
+    if (b < a) return select_2_4_ab(b, a, c, d);
+    return            select_2_4_ab(a, b, c, d);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& select_2_4(T& a,
+              T& b,
+              T& c,
+              T& d)
+{
+    if (b < a) return select_2_4_ab(b, a, c, d);
+    return            select_2_4_ab(a, b, c, d);
+}
+
+template<typename T>
+bool stable_cmp(T x, T y, int ix, int iy)
+{
+    assert(ix != iy);
+    if (ix < iy) return x < y;
+    return !(y < x);
+}
+
+template<int ia, int ib, typename T>
+    requires(TotallyOrdered(T))
+const T& select_0_2(const T& a,
+                    const T& b)
+{
+    if (stable_cmp(b, a, ib, ia)) return b;
+    return a;
+}
+
+template<int ia, int ib, typename T>
+    requires(TotallyOrdered(T))
+T& select_0_2(T& a,
+              T& b)
+{
+    if (stable_cmp(b, a, ib, ia)) return b;
+    return a;
+}
+
+template<int ia, int ib, int ic, int id, typename T>
+    requires(TotallyOrdered(T))
+const T& select_1_4_ab_cd(const T& a,
+                          const T& b,
+                                  const T& c,
+                                  const T& d)
+{
+    if (stable_cmp(c, a, ic, ia)) return
+        select_0_2<ia,id,T>(a, d);
+    return
+        select_0_2<ib,ic,T>(b, c);
+}
+
+template<int ia, int ib, int ic, int id, typename T>
+    requires(TotallyOrdered(T))
+T& select_1_4_ab_cd(T& a,
+                    T& b,
+                    T& c,
+                    T& d)
+{
+    if (stable_cmp(c, a, ic, ia)) return
+        select_0_2<ia,id,T>(a, d);
+    return
+        select_0_2<ib,ic,T>(b, c);
+}
+
+template<int ia, int ib, int ic, int id, typename T>
+    requires(TotallyOrdered(T))
+const T& select_1_4_ab(const T& a,
+                       const T& b,
+                       const T& c,
+                       const T& d)
+{
+    if (stable_cmp(d, c, id, ic)) return
+        select_1_4_ab_cd<ia,ib,id,ic,T>(a, b, d, c);
+    return
+        select_1_4_ab_cd<ia,ib,ic,id,T>(a, b, c, d);
+}
+
+template<int ia, int ib, int ic, int id, typename T>
+    requires(TotallyOrdered(T))
+T& select_1_4_ab(T& a,
+                 T& b,
+                 T& c,
+                 T& d)
+{
+    if (stable_cmp(d, c, id, ic)) return
+        select_1_4_ab_cd<ia,ib,id,ic,T>(a, b, d, c);
+    return
+        select_1_4_ab_cd<ia,ib,ic,id,T>(a, b, c, d);
+}
+
+template<int ia, int ib, int ic, int id, typename T>
+    requires(TotallyOrdered(T))
+const T& select_1_4(const T& a,
+                    const T& b,
+                    const T& c,
+                    const T& d)
+{
+    if (stable_cmp(b, a, ib, ia)) return
+        select_1_4_ab<ib,ia,ic,id,T>(b, a, c, d);
+    return
+        select_1_4_ab<ia,ib,ic,id,T>(a, b, c, d);
+}
+
+template<int ia, int ib, int ic, int id, typename T>
+    requires(TotallyOrdered(T))
+T& select_1_4(T& a,
+              T& b,
+              T& c,
+              T& d)
+{
+    if (stable_cmp(b, a, ib, ia)) return
+        select_1_4_ab<ib,ia,ic,id,T>(b, a, c, d);
+    return
+        select_1_4_ab<ia,ib,ic,id,T>(a, b, c, d);
+}
+
+template<int ia, int ib, int ic, int id, int ie, typename T>
+    requires(TotallyOrdered(T))
+const T& select_2_5_ab_cd(const T& a,
+                          const T& b,
+                          const T& c,
+                          const T& d,
+                          const T& e)
+{
+    if (stable_cmp(c, a, ic, ia)) return
+        select_1_4_ab<ia, ib, id, ie, T>(a, b, d, e);
+    return
+        select_1_4_ab<ic, id, ib, ie, T>(c, d, b, e);
+}
+
+template<int ia, int ib, int ic, int id, int ie, typename T>
+    requires(TotallyOrdered(T))
+T& select_2_5_ab_cd(T& a,
+                    T& b,
+                    T& c,
+                    T& d,
+                    T& e)
+{
+    if (stable_cmp(c, a, ic, ia)) return
+        select_1_4_ab<ia, ib, id, ie, T>(a, b, d, e);
+    return
+        select_1_4_ab<ic, id, ib, ie, T>(c, d, b, e);
+}
+
+template<int ia, int ib, int ic, int id, int ie, typename T>
+    requires(TotallyOrdered(T))
+const T& select_2_5_ab(const T& a,
+                       const T& b,
+                       const T& c,
+                       const T& d,
+                       const T& e)
+{
+    if (stable_cmp(d, c, id, ic)) return
+        select_2_5_ab_cd<ia, ib, id, ic, ie, T>(a, b, d, c, e);
+    return
+        select_2_5_ab_cd<ia, ib, ic, id, ie, T>(a, b, c, d, e);
+}
+
+template<int ia, int ib, int ic, int id, int ie, typename T>
+    requires(TotallyOrdered(T))
+T& select_2_5_ab(T& a,
+                 T& b,
+                 T& c,
+                 T& d,
+                 T& e)
+{
+    if (stable_cmp(d, c, id, ic)) return
+        select_2_5_ab_cd<ia, ib, id, ic, ie, T>(a, b, d, c, e);
+    return
+        select_2_5_ab_cd<ia, ib, ic, id, ie, T>(a, b, c, d, e);
+}
+
+template<int ia, int ib, int ic, int id, int ie, typename T>
+    requires(TotallyOrdered(T))
+const T& select_2_5(const T& a,
+                    const T& b,
+                    const T& c,
+                    const T& d,
+                    const T& e)
+{
+    if (stable_cmp(b, a, ib, ia)) return
+        select_2_5_ab<ib, ia, id, ic, ie, T>(b, a, d, c, e);
+    return
+        select_2_5_ab<ia, ib, ic, id, ie, T>(a, b, c, d, e);
+}
+
+template<int ia, int ib, int ic, int id, int ie, typename T>
+    requires(TotallyOrdered(T))
+T& select_2_5(T& a,
+              T& b,
+              T& c,
+              T& d,
+              T& e)
+{
+    if (stable_cmp(b, a, ib, ia)) return
+        select_2_5_ab<ib, ia, id, ic, ie, T>(b, a, d, c, e);
+    return
+        select_2_5_ab<ia, ib, ic, id, ie, T>(a, b, c, d, e);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+inline const T& select_2_5(const T& a,
+                           const T& b,
+                           const T& c,
+                           const T& d,
+                           const T& e)
+{
+    return select_2_5<0, 1, 2, 3, 4>(a, b, c, d, e);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+inline T& select_2_5(T& a,
+                     T& b,
+                     T& c,
+                     T& d,
+                     T& e)
+{
+    return select_2_5<0, 1, 2, 3, 4>(a, b, c, d, e);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+inline const T& median_5(const T& a,
+                         const T& b,
+                         const T& c,
+                         const T& d,
+                         const T& e)
+{
+    return select_2_5<0, 1, 2, 3, 4>(a, b, c, d, e);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+inline T& median_5(T& a,
+                   T& b,
+                   T& c,
+                   T& d,
+                   T& e)
+{
+    return select_2_5<0, 1, 2, 3, 4>(a, b, c, d, e);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& my_select_2_5_abc(const T& a,
+                           const T& b,
+                           const T& c,
+                           const T& d,
+                           const T& e)
+{
+    if (d < b) {
+        if (e < b) return
+            select_2_3(a, d, e);
+        return
+            b;
+    } else {
+        if (e < b) return
+            b;
+        return
+            select_0_3(c, d, e);
+    }
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& my_select_2_5_abc(T& a,
+                     T& b,
+                     T& c,
+                     T& d,
+                     T& e)
+{
+    if (d < b) {
+        if (e < b) return
+            select_2_3(a, d, e);
+        return
+            b;
+    } else {
+        if (e < b) return
+            b;
+        return
+            select_0_3(c, d, e);
+    }
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& my_select_2_5_ab(const T& a,
+                          const T& b,
+                          const T& c,
+                          const T& d,
+                          const T& e)
+{
+    if (c < b) {
+        if (c < a) return
+            my_select_2_5_abc(c, a, b, d, e);
+        return
+            my_select_2_5_abc(a, c, b, d, e);
+    }
+    return
+        my_select_2_5_abc(a, b, c, d, e);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& my_select_2_5_ab(T& a,
+                    T& b,
+                    T& c,
+                    T& d,
+                    T& e)
+{
+    if (c < b) {
+        if (c < a) return
+            my_select_2_5_abc(c, a, b, d, e);
+        return
+            my_select_2_5_abc(a, c, b, d, e);
+    }
+    return
+        my_select_2_5_abc(a, b, c, d, e);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+const T& my_select_2_5(const T& a,
+                       const T& b,
+                       const T& c,
+                       const T& d,
+                       const T& e)
+{
+    if (b < a) return
+        my_select_2_5_ab(b, a, c, d, e);
+    return
+        my_select_2_5_ab(a, b, c, d, e);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+T& my_select_2_5(T& a,
+                 T& b,
+                 T& c,
+                 T& d,
+                 T& e)
+{
+    if (b < a) return
+        my_select_2_5_ab(b, a, c, d, e);
+    return
+        my_select_2_5_ab(a, b, c, d, e);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+inline const T& my_median_5(const T& a,
+                            const T& b,
+                            const T& c,
+                            const T& d,
+                            const T& e)
+{
+    return my_select_2_5(a, b, c, d, e);
+}
+
+template<typename T>
+    requires(TotallyOrdered(T))
+inline T& my_median_5(T& a,
+                      T& b,
+                      T& c,
+                      T& d,
+                      T& e)
+{
+    return my_select_2_5(a, b, c, d, e);
 }
 
 #endif
--- my_selection_test.cpp
+++ my_selection_test.cpp
@@ -26,6 +26,11 @@ string pluralize(string input, size_t count)
     return input + "s";
 }
 
+bool operator<(pair<int, size_t> lhs, pair<int, size_t> rhs)
+{
+    return lhs.first < rhs.first;
+}
+
 bool less_than(int a, int b)
 {
     return a < b;
@@ -68,7 +73,7 @@ public:
     bool operator()(pair<const Domain(R)&, size_t> lhs,
                     pair<const Domain(R)&, size_t> rhs)
     {
-        return lhs.first < rhs.first;
+        return r(lhs.first, rhs.first);
     }
 
 private:
@@ -195,6 +200,100 @@ void test_all_permutations(Test test_j_k,
     } while (next_permutation(v.begin(), v.end()));
 }
 
+void test_j_2_natural(string procedure_name,
+                      const pair<int, size_t>&
+                          (*select_j_2)(const pair<int, size_t>&,
+                                        const pair<int, size_t>&),
+                      const vector<pair<int, size_t> >& v,
+                      pair<int, size_t> expected)
+{
+    pair<int, size_t> actual = select_j_2(v[0], v[1]);
+    if (expected != actual) {
+        cout << "    test failed for procedure " << procedure_name << endl;
+        cout << "        a: " << v[0] << "; b: " << v[1];
+        cout << "; expected: " << expected << "; actual: " << actual << endl;
+        failures++;
+    } else {
+        successes++;
+    }
+}
+
+void test_j_3_natural(string procedure_name,
+                      const pair<int, size_t>&
+                          (*select_j_3)(const pair<int, size_t>&,
+                                        const pair<int, size_t>&,
+                                        const pair<int, size_t>&),
+                      const vector<pair<int, size_t> >& v,
+                      pair<int, size_t> expected)
+{
+    pair<int, size_t> actual = select_j_3(v[0], v[1], v[2]);
+    if (expected != actual) {
+        cout << "    test failed for procedure " << procedure_name << endl;
+        cout << "        a: " << v[0] << "; b: " << v[1] << "; c: " << v[2];
+        cout << "; expected: " << expected << "; actual: " << actual << endl;
+        failures++;
+    } else {
+        successes++;
+    }
+}
+
+void test_j_4_natural(string procedure_name,
+                      const pair<int, size_t>&
+                          (*select_j_4)(const pair<int, size_t>&,
+                                        const pair<int, size_t>&,
+                                        const pair<int, size_t>&,
+                                        const pair<int, size_t>&),
+                      const vector<pair<int, size_t> >& v,
+                      pair<int, size_t> expected)
+{
+    pair<int, size_t> actual = select_j_4(v[0], v[1], v[2], v[3]);
+    if (expected != actual) {
+        cout << "    test failed for procedure " << procedure_name << endl;
+        cout << "        a: " << v[0] << "; b: " << v[1];
+        cout  << "; c: " << v[2] << "; d: " << v[3];
+        cout << "; expected: " << expected << "; actual: " << actual << endl;
+        failures++;
+    } else {
+        successes++;
+    }
+}
+
+void test_j_5_natural(string procedure_name,
+                      const pair<int, size_t>&
+                          (*select_j_5)(const pair<int, size_t>&,
+                                        const pair<int, size_t>&,
+                                        const pair<int, size_t>&,
+                                        const pair<int, size_t>&,
+                                        const pair<int, size_t>&),
+                      const vector<pair<int, size_t> >& v,
+                      pair<int, size_t> expected)
+{
+    pair<int, size_t> actual = select_j_5(v[0], v[1], v[2], v[3], v[4]);
+    if (expected != actual) {
+        cout << "    test failed for procedure " << procedure_name << endl;
+        cout << "        a: " << v[0] << "; b: " << v[1];
+        cout  << "; c: " << v[2] << "; d: " << v[3] << "; e: " << v[4];
+        cout << "; expected: " << expected << "; actual: " << actual << endl;
+        failures++;
+    } else {
+        successes++;
+    }
+}
+
+template<typename Test, typename Select>
+void test_all_permutations(Test test_j_k,
+                           string procedure_name,
+                           Select select,
+                           vector<int> v,
+                           pair<int, size_t> expected)
+{
+    do {
+        vector<pair<int, size_t> > augmented =
+            attach_stability_indices(v);
+        test_j_k(procedure_name, select, augmented, expected);
+    } while (next_permutation(v.begin(), v.end()));
+}
+
 template<typename R, int k>
     requires(Relation(R))
 struct select_t;
@@ -227,6 +326,38 @@ struct select_t<R, 5>
     typedef const Domain(R)& (*type)(const Domain(R)&, const Domain(R)&, const Domain(R)&, const Domain(R)&, const Domain(R)&, R);
 };
 
+template<typename T, int k>
+    requires(TotallyOrdered(T))
+struct select_natural_t;
+
+template<typename T>
+    requires(TotallyOrdered(T))
+struct select_natural_t<T, 2>
+{
+    typedef const T& (*type)(const T&, const T&);
+};
+
+template<typename T>
+    requires(TotallyOrdered(T))
+struct select_natural_t<T, 3>
+{
+    typedef const T& (*type)(const T&, const T&, const T&);
+};
+
+template<typename T>
+    requires(TotallyOrdered(T))
+struct select_natural_t<T, 4>
+{
+    typedef const T& (*type)(const T&, const T&, const T&, const T&);
+};
+
+template<typename T>
+    requires(TotallyOrdered(T))
+struct select_natural_t<T, 5>
+{
+    typedef const T& (*type)(const T&, const T&, const T&, const T&, const T&);
+};
+
 void run_all_test_cases()
 {
     vector<int> v;
@@ -236,6 +367,11 @@ void run_all_test_cases()
     select_t<StabilityRelation<bool (*)(int, int)>, 4>::type s4;
     select_t<StabilityRelation<bool (*)(int, int)>, 5>::type s5;
 
+    select_natural_t<pair<int, size_t>, 2>::type sn2;
+    select_natural_t<pair<int, size_t>, 3>::type sn3;
+    select_natural_t<pair<int, size_t>, 4>::type sn4;
+    select_natural_t<pair<int, size_t>, 5>::type sn5;
+
 #define TEST_CASE(J, K, E0, E1, ...) v = { __VA_ARGS__ };\
     s##K = select_##J##_##K;\
     test_all_permutations(test_j_##K<StabilityRelation<bool (*)(int, int)> >,\
@@ -243,6 +379,12 @@ void run_all_test_cases()
                           s##K,\
                           v,\
                           strengthen(less_than),\
+                          pair<const int&, size_t>(E0, E1));\
+    sn##K = select_##J##_##K;\
+    test_all_permutations(test_j_##K##_natural,\
+                          string("select_") + #J + "_" + #K,\
+                          sn##K,\
+                          v,\
                           pair<const int&, size_t>(E0, E1));
 #define MY_TEST_CASE(J, K, E0, E1, ...) v = { __VA_ARGS__ };\
     s##K = my_select_##J##_##K;\
@@ -251,6 +393,12 @@ void run_all_test_cases()
                           s##K,\
                           v,\
                           strengthen(less_than),\
+                          pair<const int&, size_t>(E0, E1));\
+    sn##K = my_select_##J##_##K;\
+    test_all_permutations(test_j_##K##_natural,\
+                          string("my_select_") + #J + "_" + #K,\
+                          sn##K,\
+                          v,\
                           pair<const int&, size_t>(E0, E1));
 #include "my_selection_test_cases.h"
 #undef TEST_CASE
