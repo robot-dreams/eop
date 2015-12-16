90cdfe8d12a10973e20ecc315ed094349ade64ed
Elliott Jin <elliott.jin@gmail.com>
2015-12-16 03:56:56 -0800

    Dedup division code

--- ch5-division.cpp
+++ ch5-division.cpp
@@ -1,5 +1,6 @@
 #include <cmath>
 #include <iostream>
+#include "my_division.h"
 #include "my_integer.h"
 #include "my_intrinsics.h"
 #include "my_type_functions.h"
@@ -38,57 +39,6 @@ QuotientType(T) slow_quotient(T a, T b)
 
 template<typename T>
     requires(ArchimedeanMonoid(T))
-T remainder_recursive(T a, T b)
-{
-    // Precondition:
-    //     a >= b > 0
-    if (a - b >= b) {
-        a = remainder_recursive(a, b + b);
-        if (a < b) return a;
-    }
-    return a - b;
-}
-
-template<typename T>
-    requires(ArchimedeanMonoid(T))
-T remainder_nonnegative(T a, T b)
-{
-    // Precondition:
-    //     a >= 0 && b > 0
-    if (a < b) return a;
-    return remainder_recursive(a, b);
-}
-
-template<typename T>
-    requires(ArchimedeanMonoid(T))
-T largest_doubling(T a, T b)
-{
-    // Precondition:
-    //     a >= b > 0
-    while (b <= a - b) b = b + b;
-    return b;
-    // Postcondition:
-    //     b <= a < b + b
-}
-
-template<typename T>
-    requires(HalvableMonoid(T))
-T remainder_nonnegative_iterative(T a, T b)
-{
-    // Precondition:
-    //     a >= 0 && b > 0
-    if (a < b) return a;
-    T c = largest_doubling(a, b);
-    a = a - c;
-    do {
-        c = half(c);
-        if (a >= c) a = a - c;
-    } while (a >= b);
-    return a;
-}
-
-template<typename T>
-    requires(ArchimedeanMonoid(T))
 T subtractive_gcd_nonzero(T a, T b)
 {
     // Precondition:
@@ -111,20 +61,6 @@ T subtractive_gcd(T a, T b)
         while (b <= a) a = a - b;
         if (a == T(0)) return b;
         while (a <= b) b = b - a;
-    }
-}
-
-template<typename T>
-    requires(EuclideanMonoid(T))
-T fast_subtractive_gcd(T a, T b)
-{
-    // Precondition:
-    //     a >= 0 && b >= 0 && !(a == 0 && b == 0)
-    while (true) {
-        if (b == T(0)) return a;
-        a = remainder_nonnegative(a, b);
-        if (a == T(0)) return b;
-        b = remainder_nonnegative(b, a);
     }
 }
 
@@ -174,44 +110,6 @@ pair<QuotientType(T), T> largest_doubling_2(T a, T b)
 }
 
 template<typename T>
-    requires(ArchimedeanMonoid(T))
-pair<QuotientType(T), T> quo_rem_nonnegative(T a, T b)
-{
-    // Precondition:
-    //     a >= 0 && b > 0
-    typedef QuotientType(T) N;
-    if (a < b) return     pair<N, T>(N(0), a);
-    if (a - b < b) return pair<N, T>(N(1), a - b);
-    pair<N, T> q = quo_rem_nonnegative(a, b + b);
-    N m = twice(q.first);
-    a = q.second;
-    if (a < b) return pair<N, T>(m, a);
-    return            pair<N, T>(successor(m), a - b);
-}
-
-template<typename T>
-    requires(HalvableMonoid(T))
-pair<QuotientType(T), T> quo_rem_nonnegative_iter(T a, T b)
-{
-    // Precondition:
-    //     a >= 0 && b > 0
-    typedef QuotientType(T) N;
-    if (a < b) return pair<N, T>(N(0), a);
-    T c = largest_doubling(a, b);
-    a = a - c;
-    N n(1);
-    while (c != b) {
-        n = twice(n);
-        c = half(c);
-        if (a >= c) {
-            a = a - c;
-            n = successor(n);
-        }
-    }
-    return pair<N, T>(n, a);
-}
-
-template<typename T>
     requires(HalvableMonoid(T))
 pair<QuotientType(T), T> quo_rem_nonnegative_iter_2(T a, T b)
 {
@@ -238,32 +136,6 @@ pair<QuotientType(T), T> quo_rem_nonnegative_iter_2(T a, T b)
 template<typename Op>
     requires(BinaryOperation(Op) &&
         ArchimedeanGroup(Domain(Op)))
-Domain(Op) remainder(Domain(Op) a, Domain(Op) b, Op rem)
-{
-    // Precondition:
-    //     b != 0
-    typedef Domain(Op) T;
-    T r;
-    if (a < T(0))
-        if (b < T(0)) {
-            r = -rem(-a, -b);
-        } else {
-            r = rem(-a, b);
-            if (r != T(0)) r = b - r;
-        }
-        else
-            if (b < T(0)) {
-                r = rem(a, -b);
-                if (r != T(0)) r = b + r;
-            } else {
-                r = rem(a, b);
-            }
-    return r;
-}
-
-template<typename Op>
-    requires(BinaryOperation(Op) &&
-        ArchimedeanGroup(Domain(Op)))
 Domain(Op) remainder_2(Domain(Op) a, Domain(Op) b, Op rem)
 {
     // Precondition:
@@ -276,41 +148,6 @@ Domain(Op) remainder_2(Domain(Op) a, Domain(Op) b, Op rem)
         a = a + c;
     }
     return rem(a, b);
-}
-
-template<typename F>
-    requires(HomogeneousFunction(F) && Arity(F) == 2 &&
-        ArchimedeanGroup(Domain(F)) &&
-        Codomain(F) == pair<QuotientType(Domain(F)),
-                            Domain(F)>)
-pair<QuotientType(Domain(F)), Domain(F)>
-quotient_remainder(Domain(F) a, Domain(F) b, F quo_rem)
-{
-    // Precondition:
-    //     b != 0
-    typedef Domain(F) T;
-    pair<QuotientType(T), T> q_r;
-    if (a < T(0)) {
-        if (b < T(0)) {
-            q_r = quo_rem(-a, -b); q_r.second = -q_r.second;
-        } else {
-            q_r = quo_rem(-a, b);
-            if (q_r.second != T(0)) {
-                q_r.second = b - q_r.second; q_r.first = successor(q_r.first);
-            }
-            q_r.first = -q_r.first;
-        }
-    } else {
-        if (b < T(0)) {
-            q_r = quo_rem(a, -b);
-            if (q_r.second != T(0)) {
-                q_r.second = b + q_r.second; q_r.first = successor(q_r.first);
-            }
-            q_r.first = -q_r.first;
-        } else
-            q_r = quo_rem(a, b);
-    }
-    return q_r;
 }
 
 template<typename F>
